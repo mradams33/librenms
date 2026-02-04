8ee5235efc0ba92e7eeb3e311475b0d61583d1d6
mradams33 <mradams@ilstu.edu>
2026-02-04 11:55:42 -0600

    Forgot to update the index to unique in the test script.

--- database/migrations/2025_12_05_101900_add_ports_voice_vlan_table.php
+++ database/migrations/2025_12_05_101900_add_ports_voice_vlan_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->integer('device_id')->unsigned();
             $table->integer('voice_vlan')->unsigned()->default(0);
             $table->unique(['device_id', 'port_id', 'voice_vlan'], 'ports_voice_vlans_device_id_port_id_voice_vlan_unique');
-            $table->index(['port_id'], 'ports_voice_vlans_port_id_index');
+            $table->unique(['port_id'], 'ports_voice_vlans_port_id_index');
         });
     }
 
